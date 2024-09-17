228cb477955fc64f06e1486cdf621afc62726700
lavrt <lavrik-aleksey-1@yandex.ru>
2024-09-17 20:17:14 +0300

    Update my_sort.cpp

--- my_sort.cpp
+++ my_sort.cpp
@@ -10,10 +10,11 @@
 #define FREE(ptr_) \
     do { free(ptr_); ptr_ = NULL; } while (0)
 
-void bubble_sort(void * array, int number_of_strings, int compare_strings_direct(void * a, void * b), size_t element_size);
+void bubble_sort(void * array, int number_of_strings, int comparator(void * a, void * b), size_t element_size);
 int compare_strings_direct(void * first_string_, void * second_string_);
 int compare_strings_reverse(void * first_string_, void * second_string_);
 void exchanging_strings(char ** array, int first_array, int second_array);
+void text_to_file(char ** strings, FILE * file_name, int number_of_strings);
 
 int main (void)
 {
@@ -24,8 +25,11 @@ int main (void)
     FILE * text_input = fopen("onegin.txt", "r");
     if (text_input == NULL) { FILE_ERROR("onegin.txt"); }
 
-    FILE * text_output_sorted = fopen("onegin_sorted.txt", "w");
-    if (text_output_sorted == NULL) { FILE_ERROR("onegin_sorted.txt"); }
+    FILE * text_output_sorted_direct = fopen("onegin_sorted_direct.txt", "w");
+    if (text_output_sorted_direct == NULL) { FILE_ERROR("onegin_sorted_direct.txt"); }
+
+    FILE * text_output_sorted_reverse = fopen("onegin_sorted_reverse.txt", "w");
+    if (text_output_sorted_reverse == NULL) { FILE_ERROR("onegin_sorted_reverse.txt"); }
 
     char * buffer = (char *)calloc(size + 1, sizeof(char));
     char ** strings = (char **)calloc(1, sizeof(char *));
@@ -47,12 +51,14 @@ int main (void)
     }
 
     bubble_sort(strings, number_of_strings, compare_strings_direct, sizeof(char *));
+    text_to_file(strings, text_output_sorted_direct, number_of_strings);
 
-    for (int i = 0; i < number_of_strings; i++)
-    {
-        fputs(strings[i], text_output_sorted);
-        fputc('\n', text_output_sorted);
-    }
+    bubble_sort(strings, number_of_strings, compare_strings_reverse, sizeof(char *));
+    text_to_file(strings, text_output_sorted_reverse, number_of_strings);
+
+    fclose(text_input);
+    fclose(text_output_sorted_direct);
+    fclose(text_output_sorted_reverse);
 
     FREE(buffer);
     FREE(strings);
@@ -60,13 +66,13 @@ int main (void)
     return 0;
 }
 
-void bubble_sort(void * array, int number_of_strings, int compare_strings_direct(void * a, void * b), size_t element_size)
+void bubble_sort(void * array, int number_of_strings, int comparator(void * a, void * b), size_t element_size)
 {
     for (int j = 0; j < number_of_strings; j++)
     {
         for (int i = 0; i < number_of_strings - 1; i++)
         {
-            if (compare_strings_direct(array + i * element_size, array + (i + 1) * element_size))
+            if (comparator(array + i * element_size, array + (i + 1) * element_size))
             {
                 exchanging_strings((char **)array, i, i + 1);
             }
@@ -117,4 +123,13 @@ void exchanging_strings(char ** array, int first_array, int second_array)
     char * temp = array[first_array];
                   array[first_array] = array[second_array];
                                        array[second_array] = temp;
+}
+
+void text_to_file(char ** strings, FILE * file_name, int number_of_strings)
+{
+    for (int i = 0; i < number_of_strings; i++)
+    {
+        fputs(strings[i], file_name);
+        fputc('\n', file_name);
+    }
 }
